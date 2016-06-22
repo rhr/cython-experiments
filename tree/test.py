f9ad6359de9f5d3bfc6d81ad344267e5ced6baa8
Rick Ree <github@rree.fastmail.fm>
2016-06-22 13:59:32 +0900

    initial commit

---
+++ tree/test.py
@@ -0,0 +1,18 @@
+import newick, tree
+import numpy as np
+
+s = '((((Homo:0.21,Pongo:0.21)A:0.28,Macaca:0.49)B:0.13,Ateles:0.62)C:0.38,Galago:1.00)root;'
+r = newick.parse(s)
+t = tree.Tree(r)
+a = np.empty(t.nnodes, dtype=int)
+tree.cladesizes3(t, a)
+print a
+
+
+
+
+
+
+
+
+
